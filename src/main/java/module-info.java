bd32db7cce6f9cdf919d4d0818b26f4957f5c71c
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2022-10-24 12:23:46 +0200

    module-info fixed

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,3 @@
-module com.bensler.sz {
-
-requires java.desktop;
-requires jgoodies.looks;
-requires jgoodies.forms;
-requires com.google.zxing;
-requires com.google.zxing.javase;
+module com.bensler.suso {
 
 }
