4bc17fe646496c5668cd851e1c0944402547d706
Thomas Bensler <1121613+bensler@users.noreply.github.com>
2023-01-25 07:24:48 +0100

    getBounds() added for rendering

--- src/main/java/com/bensler/suso/Constraint.java
+++ src/main/java/com/bensler/suso/Constraint.java
@@ -1,5 +1,6 @@
 package com.bensler.suso;
 
+import java.awt.Rectangle;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -27,6 +28,15 @@ public class Constraint {
 
   public Set<Digit> getUsedDigits(Field field) {
     return coordinates.stream().flatMap(coordinate -> field.getDigit(coordinate).stream()).collect(Collectors.toSet());
+  }
+
+  public Rectangle getBounds() {
+    final int minX = coordinates.stream().mapToInt(Coordinate::getX).min().getAsInt();
+    final int maxX = coordinates.stream().mapToInt(Coordinate::getX).max().getAsInt();
+    final int minY = coordinates.stream().mapToInt(Coordinate::getY).min().getAsInt();
+    final int maxY = coordinates.stream().mapToInt(Coordinate::getY).max().getAsInt();
+
+    return new Rectangle(minX, minY, maxX - minX, maxY - minY);
   }
 
 }
